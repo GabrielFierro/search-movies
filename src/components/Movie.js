79feb28cc38a8534adf1ba78b97d850018a232d4
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-10-13 19:42:18 -0300

    fix: fix the properties for the Box component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,7 +4,7 @@ const Movie = ({ movie }) => (
 	// This component display a box with the title, year and the poster
 	// of the searched movie
 	<Flex flexWrap border="3px" borderColor="tomato">
-		<Box w="400px" h="100%" mt={12} ml={12}>
+		<Box w="400px" h="100%" my={8}>
 			<Text color="white">{movie.Title}</Text>
 			<Text color="white">{movie.Year}</Text>
 			<img alt={movie.Title} src={movie.Poster} w="300px" />
