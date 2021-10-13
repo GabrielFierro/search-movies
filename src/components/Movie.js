94b017a659ee0908d677b799b0bf4a40cb9a7435
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-10-13 18:23:20 -0300

    add: add the year below of the title

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,7 @@ const Movie = ({ movie }) => (
 	<Flex flexWrap border="3px" borderColor="tomato">
 		<Box w="400px" h="100%" mt={12} ml={12}>
 			<Text color="white">{movie.Title}</Text>
+			<Text color="white">{movie.Year}</Text>
 			<img alt={movie.Title} src={movie.Poster} w="300px" />
 		</Box>
 	</Flex>
