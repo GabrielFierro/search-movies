7eb204e7371a6130b90f750bcf8ff4b6d1088742
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-10-13 19:43:39 -0300

    add: add a parent div to display all the results and a child div to display each movie with its own properties

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,11 +1,19 @@
 import Movie from "./Movie.js";
 
-// This function return a component Movie with the props movie, 
+// This function return a component Movie with the props movie,
 // that represent each element of the array with its own key.
 function MoviesList({ results }) {
-	return results.map((movie) => {
-		return <Movie movie={movie} key={movie.imdbID} />;
-	});
+	return (
+		<div className="MoviesList">
+			{results.map((movie) => {
+				return (
+					<div key={movie.imdbID} className="MoviesList-item">
+						<Movie movie={movie} />;
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default MoviesList;
