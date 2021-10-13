e51d9dd7091bfef99d6a43cea3e8cf5259ac879f
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-10-13 18:24:12 -0300

    add: add a comment that describe the the function of the Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,8 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 
 const Movie = ({ movie }) => (
+	// This component display a box with the title, year and the poster
+	// of the searched movie
 	<Flex flexWrap border="3px" borderColor="tomato">
 		<Box w="400px" h="100%" mt={12} ml={12}>
 			<Text color="white">{movie.Title}</Text>
