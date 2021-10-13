ad9c580777e1a58eb48d892825b8b3ae31f40af3
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-10-13 18:38:34 -0300

    del: delete the width and height properties, and add the size propertie in the Button component

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -3,14 +3,13 @@ import { Button } from "@chakra-ui/react";
 function SearchForm({ children }) {
 	return (
 		<Button
-			w="75px"
-			h="45px"
-			mt="-5px"
+			mt="-4px"
 			ml="10px"
 			color="white"
 			bg="#db0000"
 			variant="solid"
 			_hover={{ bg: "#aa0000" }}
+			size="lg"
 		>
 			{children}
 		</Button>
