cc95d5911010eff642ee6f06aa018294fe02c1cf
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-11-04 20:55:05 -0300

    add the import for React and add a the tag a to link the movies by it link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
+import React from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
 
 const Movie = ({ movie }) => (
 	// This component display a box with the title, year and the poster
 	// of the searched movie
-	<Flex flexWrap border="3px" borderColor="tomato">
-		<Box w="400px" h="100%" my={8}>
-			<Text color="white">{movie.Title}</Text>
-			<Text color="white">{movie.Year}</Text>
-			<img alt={movie.Title} src={movie.Poster} w="300px" />
-		</Box>
-	</Flex>
+	<a href={`?id=${movie.imdbID}`}>
+		<Flex flexWrap border="3px" borderColor="tomato">
+			<Box w="400px" h="100%" my={8}>
+				<Text color="white">{movie.Title}</Text>
+				<Text color="white">{movie.Year}</Text>
+				<img alt={movie.Title} src={movie.Poster} w="300px" />
+			</Box>
+		</Flex>
+	</a>
 );
 
 export default Movie;
