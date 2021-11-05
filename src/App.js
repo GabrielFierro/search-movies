d49bb7ce4849eed6449fd43936b0ce4a27a20bde
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-11-04 21:10:45 -0300

    del: delete unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
-// Import for components
-import MoviesList from "./components/MoviesList";
-import Title from "./components/Title";
-import SearchForm from "./components/SearchForm/SearchForm";
 // Import for the pagesz
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 // Import for the CSS code
 import "./App.css";
-// Import for chakra-ui component
-import { Box, Center, Text } from "@chakra-ui/react";
 
 function App() {
 	const url = new URL(document.location);
