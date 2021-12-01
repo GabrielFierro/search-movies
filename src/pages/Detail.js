bcaef658896bb3c956f4b51db513bfd9f6264c33
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-12-01 20:11:06 -0300

    del: delete the id parameter in the function Detail

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,7 +3,7 @@ import { Heading, Text } from "@chakra-ui/react";
 
 const API_KEY = "234ddc1c"; // Create a const variable with the key from the API
 
-export default function Detail({ id }) {
+export default function Detail() {
 	const [showData, setShowData] = useState(true);
 	const [movie, setMovie] = useState({});
 	const { Title, Poster, Actors, Metascore, Plot } = movie;
